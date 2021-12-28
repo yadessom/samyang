32c65fc6848b7d486f2b3633f7b31c7c2a26a12a
yadess <90118673+yadessom@users.noreply.github.com>
2021-12-28 19:31:23 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(function(){
+
+    $('.visual').slick({
+        autoplay: true, //슬라이드 자동 실행
+        dots:true, //페이지 버튼 보이기
+        fade:true, //화면전환 방식
+        autoplaySpeed:3000, //자동 전환 속도
+      });
+
+    // *******************************************************
+
+    $('.gnb').mouseenter(function(){
+        $('#header').stop().animate({'height':411},250);
+        $('.depth2').slideDown(250);
+    });
+
+    $('#header').mouseleave(function(){
+        $('#header').stop().animate({'height':144},250);
+        $('.depth2').slideUp(250);
+    });
+
+    // *******************************************************
+
+    $('.new_product_slide').slick({
+        autoplay:true,
+        dots:true,
+        arrows:false,
+        autoplaySpeed:5000
+      });
+
+      // *******************************************************
+
+      $('.sec_01_list li').click(function(){
+          $(this).addClass('on');
+          $(this).siblings().removeClass('on');
+      });
+
+      // *******************************************************
+
+      $('.sec_01_slide').slick({
+        // autoplay: true, //슬라이드 자동 실행
+        dots:true, //페이지 버튼 보이기
+        fade:true, //화면전환 방식
+        autoplaySpeed:3000, //자동 전환 속도
+      });
+
+
+
+
+
+
+
+});
